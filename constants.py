2a0471236f0a86f4be1e232b4c05c3aaa28c4f02
cyrus01337 <cyrus21337@gmail.com>
2020-05-18 08:21:42 +0100

    Transfer Hastebin as owner.py helper coroutine

--- constants.py
+++ constants.py
@@ -1,6 +1,14 @@
 """Explanation"""
 from enum import Enum
 
+HEADERS = {
+    "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64; "
+                   "rv:76.0) Gecko/20100101 Firefox/76.0,"),
+    "Accept-Encoding": "gzip, deflate",
+    "Accept": "*/*",
+    "Connection": "keep-alive",
+}
+
 
 class Owner(Enum):
     CYRUS = 668906205799907348
