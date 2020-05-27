9ee14a913b0c16f5e81d7f4096d5ca5f2d721b9c
cyrus01337 <cyrus21337@gmail.com>
2020-05-27 23:11:23 +0100

    Escape mentions, prevent abuse

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,15 +1,16 @@
 """Explanation"""
+import discord
 from discord.ext import commands
 
 
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
     async def say(self, ctx, *, anything):
         """Explanation"""
-        await ctx.send(anything)
+        await ctx.send(discord.utils.escape_mentions(anything))
 
 
 def setup(bot):
