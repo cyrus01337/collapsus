6f261f93ec88bb32ee299d828915e55db3dd45cc
cyrus01337 <cyrus21337@gmail.com>
2020-05-27 23:11:51 +0100

    Add prefix command

--- cogs/info.py
+++ cogs/info.py
@@ -2,6 +2,7 @@
 from discord.ext import commands
 
 import emojis
+import prefix
 
 
 class Information(commands.Cog):
@@ -13,6 +14,13 @@ class Information(commands.Cog):
         """Explanation"""
         await ctx.send(f"{emojis.PING_PONG} **Pong!** "
                        f"{int(self.bot.latency * 1000):,}ms.")
+
+    @commands.command(name="prefix")
+    async def _prefix(self, ctx):
+        """Explanation"""
+        await ctx.send(f"The prefix is `{prefix.DEFAULT}` or you can mention "
+                       f"me! E.g. `{prefix.DEFAULT} ping` or "
+                       f"{self.bot.mention}` ping`")
 
 
 def setup(bot):
