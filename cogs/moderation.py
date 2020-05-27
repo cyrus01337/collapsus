7a30dfa4c1282acdcd7e75335c9be9b710333969
cyrus01337 <cyrus21337@gmail.com>
2020-05-27 23:31:54 +0100

    Add new cog

---
+++ cogs/moderation.py
@@ -0,0 +1,28 @@
+"""Explanation"""
+from discord.ext import commands
+
+import prefix
+
+
+class ModerationCog(commands.Cog, name="Moderation"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def cog_check(self, ctx):
+        return True
+
+    @commands.group(name="settings", invoke_without_subcommand=True)
+    async def _settings(self, ctx):
+        """Explanation"""
+        message = ""
+        settings = {
+            "prefix": f"`{prefix.DEFAULT}`"
+        }
+
+        for key, value in settings.items():
+            message += f"**{key.title()}**: {value}\n"
+        await ctx.send(message)
+
+
+def setup(bot):
+    bot.add_cog(ModerationCog(bot))
