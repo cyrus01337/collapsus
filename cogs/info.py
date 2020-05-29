71cc3c7b789fed7b9ab631e8059360f5634876a2
cyrus01337 <cyrus21337@gmail.com>
2020-05-29 22:56:18 +0100

    Resolve command

--- cogs/info.py
+++ cogs/info.py
@@ -1,4 +1,6 @@
 """Explanation"""
+import typing
+
 from discord.ext import commands
 
 import emojis
@@ -8,12 +10,32 @@ import prefix
 class Information(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.data = {
+            "chest": ("SABCDEFGHI")[::-1],
+            "grotto": ("SABCDEFGHIJK")[::-1]
+        }
 
-    @commands.command()
-    async def ping(self, ctx):
-        """Explanation"""
-        await ctx.send(f"{emojis.PING_PONG} **Pong!** "
-                       f"{int(self.bot.latency * 1000):,}ms.")
+    async def _resolve(self, ctx, medium: str, index: typing.Union[int, str]):
+        medium = medium.lower()
+        data = self.data.get(medium)
+        message = "**Rank**: `{letter} ({number})`"
+
+        if medium not in self.data.keys():
+            return
+
+        if isinstance(index, int) and index > 0 and index < 13:
+            letter = data[index-1]
+            message = message.format(letter=letter, number=index)
+        elif isinstance(index, str):
+            index = index.upper()
+
+            if index not in data:
+                return
+            number = data.index(index) + 1
+            message = message.format(letter=index, number=number)
+        else:
+            return
+        await ctx.send(message)
 
     @commands.command(name="prefix")
     async def _prefix(self, ctx):
@@ -21,6 +43,17 @@ class Information(commands.Cog):
         await ctx.send(f"The prefix is `{prefix.DEFAULT}` or you can mention "
                        f"me! E.g. `{prefix.DEFAULT} ping` or "
                        f"{self.bot.user.mention}` ping`")
+
+    @commands.command()
+    async def ping(self, ctx):
+        """Explanation"""
+        await ctx.send(f"{emojis.PING_PONG} **Pong!** "
+                       f"{int(self.bot.latency * 1000):,}ms.")
+
+    @commands.group(invoke_without_subcommand=True)
+    async def resolve(self, ctx, medium, index: typing.Union[int, str]):
+        """Explanation"""
+        await self._resolve(ctx, medium=medium, index=index)
 
 
 def setup(bot):
