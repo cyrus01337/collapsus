45bb925a8af7709d8662ae47b961134517e1122e
cyrus01337 <cyrus21337@gmail.com>
2020-06-13 10:17:02 +0100

    Add script for easier setup

---
+++ scripts/unix/deploy.sh
@@ -0,0 +1,7 @@
+cd dq9-grotto-bot
+git pull
+pip install -U -r requirements.txt
+python bot.py
+git add quotes.db
+git commit -m "Update entries"
+git push
--- scripts/unix/run.sh
+++
@@ -1,3 +0,0 @@
-#!usr/bin/bash
-cd ../..
-python3.8 bot.py
