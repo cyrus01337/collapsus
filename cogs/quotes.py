7f8e35627b82c7c50ccc1c6a57733e606695b818
cyrus01337 <cyrus21337@gmail.com>
2020-08-13 21:31:40 +0100

    Patch unrecognised parameter

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -39,7 +39,7 @@ class QuotesCog(commands.Cog, name="Quotes System"):
     def cog_unload(self):
         self.bot.loop.create_task(self.db.close())
 
-    @flags.add_flag("--name", nargs="*")
+    @flags.add_flag("name", nargs="*")
     @flags.add_flag("--raw", action="store_true")
     @flags.group(aliases=["q"], usage=f"{prefix.DEFAULT}quote test",
                  invoke_without_command=True)
