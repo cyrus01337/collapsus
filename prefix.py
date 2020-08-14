3f4e79537707fd7ca89fd4aa6ffdafe1f4dc97c6
cyrus01337 <cyrus21337@gmail.com>
2020-08-14 18:41:41 +0100

    Rework prefix system

--- prefix.py
+++ prefix.py
@@ -1,16 +1,6 @@
 """Explanation"""
 from discord.ext import commands
 
-with open("./resources/_prefix", "r") as f:
-    DEFAULT = str.strip(f.read())
 
-
-def as_kwarg():
-    return dict(command_prefix=commands.when_mentioned_or(DEFAULT))
-
-
-def update(prefix: str):
-    # DEFAULT = prefix
-
-    with open("./resources/_prefix", "w") as f:
-        f.write(f"{prefix}\n")
+async def get_prefix(bot, message):
+    return commands.when_mentioned_or(bot.settings.get("prefix"))
