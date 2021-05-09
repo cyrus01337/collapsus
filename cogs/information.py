7272a53aa0cb776bcd1442e61ad97c20cd24cb8d
cyrus01337 <cyrus21337@gmail.com>
2021-05-09 19:03:37 +0100

    Implement embed settings

--- cogs/information.py
+++ cogs/information.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
 from discord.ext import commands
 
+import checks
 from base import custom
 
 
@@ -7,24 +10,41 @@ class Information(custom.Cog):
     class Setting(commands.Converter):
         async def convert(self, ctx, argument):
             argument = argument.lower()
-            settings = ctx.bot.db.Settings
 
-            if argument in settings.VALID:
+            if argument in ctx.bot.db.VALID_SETTINGS:
                 return argument
-            raise commands.BadArgument(f'invalid setting "{argument}"')
+            raise commands.BadArgument(f'{argument} is an invalid setting')
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def settings(self, ctx, setting: Setting, value: bool):
-        settings_found = self.bot.db.settings.get(ctx.author.id, None)
+    async def cog_check(self, ctx):
+        return checks.require_settings(ctx)
 
-        if not settings_found:
-            pass
-        settings_found[setting] = value
+    @commands.group(aliases=["setting"], invoke_without_command=True)
+    async def settings(self,
+                       ctx,
+                       setting: Optional[Setting] = None,
+                       value: Optional[bool] = None):
+        translation = {True: "on"}
 
-        await ctx.send(f"Successfully set setting.{setting} to {value}")
+        if not setting and not value:
+            settings_found = self.bot.db.settings[ctx.author.id]
+            embed = custom.Embed(title="Settings", desc=str(ctx.author))
+
+            for name, value in settings_found:
+                name = name.title()
+
+                if isinstance(value, bool):
+                    value = translation.get(value, "off")
+
+                embed.add_field(name=name, value=value)
+            await ctx.send(embed=embed)
+        else:
+            settings_found = self.bot.db.settings[ctx.author.id]
+            settings_found[setting] = value
+
+            await ctx.send(f"Successfully set `settings.{setting}` to {value}")
 
 
 def setup(bot):
